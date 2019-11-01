e77c91b9a7b9e818ef539df9557600b98945fd1a
Guillermo Basoco <blemished_lotus@hotmail.com>
2019-11-01 14:07:04 -0600

    Update best_answer_id on questions table when answer removed

--- app/Answer.php
+++ app/Answer.php
@@ -23,7 +23,13 @@ class Answer extends Model
       });
 
       static::deleted(function($answer){
-          $answer->question->decrement('answers_count');
+          $question = $answer->question;
+          $question->decrement('answers_count');
+          if($question->best_answer_id == $answer->id){
+            $question->best_answer_id = NULL;
+            $question->save();
+          }
+
       });
 
 
@@ -44,6 +50,12 @@ class Answer extends Model
     {
       return $this->created_at->diffForHumans();
     }
+
+    public function getStatusAttribute()
+    {
+      return $this->id == $this->question->best_answer_id ? 'vote-accepted' : '';
+    }
+
 
     public function user()
     {
