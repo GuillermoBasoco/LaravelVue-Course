bbe4d45f6e0212e1675b3e8f5627481067ac9aae
Guillermo Basoco <blemished_lotus@hotmail.com>
2019-11-01 12:54:27 -0600

    Display answers for question

--- app/Answer.php
+++ app/Answer.php
@@ -6,6 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Answer extends Model
 {
+
+    /**
+    * The attributes that are mass assignable.
+    *
+    * @var array
+    */
+    protected $fillable = ['body','user_id'];
+
     public static function boot()
     {
       parent::boot();
--- app/Http/Controllers/AnswerController.php
+++ app/Http/Controllers/AnswerController.php
@@ -3,29 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Answer;
+use App\Question;
 use Illuminate\Http\Request;
 
 class AnswerController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
 
     /**
      * Store a newly created resource in storage.
@@ -33,20 +15,16 @@ class AnswerController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(Question $question, Request $request)
     {
-        //
-    }
+        $request->validate([
+          'body' => 'required'
+        ]);
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Answer  $answer
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Answer $answer)
-    {
-        //
+        $question->answers()->create(['body' => $request->body, 'user_id' => \Auth::id()]);
+
+        return back()->with('success',"Your answer has been submitted successfully");
+
     }
 
     /**
