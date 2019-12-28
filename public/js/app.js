213e3758676bfca5d9b944de3d9cdb9f9d8f7013
Guillermo Basoco <blemished_lotus@hotmail.com>
2019-12-28 18:04:26 -0500

    Vue event on answer deletion

--- public/js/app.js
+++ public/js/app.js
@@ -3855,9 +3855,7 @@ __webpack_require__.r(__webpack_exports__);
       if (confirm('Are you sure?')) {
         axios["delete"](this.endpoint).then(function (res) {
           $(_this2.$el).fadeOut(500, function () {
-            _this2.$toast.success(res.data.message, "Success", {
-              timeout: 3000
-            });
+            _this2.$emit('deleted');
           });
         });
       }
@@ -3940,6 +3938,10 @@ function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) { for (var i = 0, arr
 
         _this.nextUrl = data.next_page_url;
       });
+    },
+    remove: function remove(index) {
+      this.answers.splice(index, 1);
+      this.count--;
     }
   },
   computed: {
@@ -40323,10 +40325,15 @@ var render = function() {
                 _vm._v(" "),
                 _c("hr"),
                 _vm._v(" "),
-                _vm._l(_vm.answers, function(answer) {
+                _vm._l(_vm.answers, function(answer, index) {
                   return _c("answer", {
                     key: answer.id,
-                    attrs: { answer: answer }
+                    attrs: { answer: answer },
+                    on: {
+                      deleted: function($event) {
+                        return _vm.remove(index)
+                      }
+                    }
                   })
                 }),
                 _vm._v(" "),
